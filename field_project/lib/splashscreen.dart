432f29468dbbd625834f5617107ecdedda5db629
MrOmarJnr <kwekuappiahjnr8@gmail.com>
2024-07-01 12:22:57 +0000

    splash with image

--- field_project/lib/splashscreen.dart
+++ field_project/lib/splashscreen.dart
@@ -1,18 +1,40 @@
+import 'package:field_project/home.dart';
+import 'package:field_project/main.dart';
 import 'package:flutter/material.dart';
 
-class Splash extends StatelessWidget {
+
+
+class Splash extends StatefulWidget {
   const Splash({super.key});
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(Duration(milliseconds: 2000), () {});
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => MyHomePage(title: 'GFG',)));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Container(
-        child: Text(
-          'Splash Screen',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: Center(
+        child: Container(
+          child: Text(
+            'Splash Screen',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
-    ));
+    );
   }
 }
