80a28ab9d47ff506a4df0c68fb510e9f164539e0
MrOmarJnr <kwekuappiahjnr8@gmail.com>
2024-07-01 15:54:20 +0000

    change13

--- field_project/lib/splashscreen.dart
+++ field_project/lib/splashscreen.dart
@@ -19,7 +19,7 @@ class _SplashState extends State<Splash> {
   _navigatetohome() async {
     await Future.delayed(Duration(milliseconds: 5000), () {});
     Navigator.pushReplacement(context,
-        MaterialPageRoute(builder: (context) => MyBottomNavigationBar()));
+        MaterialPageRoute(builder: (context) => MyHomePage(title: '',)));
   }
 
   @override
