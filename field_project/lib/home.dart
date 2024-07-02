93e02ce483b7539fa90d8d82a4caa9014700e21e
MrOmarJnr <kwekuappiahjnr8@gmail.com>
2024-07-02 11:18:20 +0000

    change22

--- field_project/lib/home.dart
+++ field_project/lib/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:field_project/Api_service.dart';
+import 'package:field_project/user_model.dart';
+import 'package:field_project/search.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -8,9 +11,10 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
-
+   FetchUserList _userList = FetchUserList();
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+
 
   void _incrementCounter() {
     setState(() {
@@ -89,16 +93,85 @@ class SearchScreen extends StatelessWidget {
 class ListScreen extends StatelessWidget {
   const ListScreen({super.key});
 
-  @override
+@override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('List Screen'),
-      ),
-      body: const Center(
-        child: Text(
-          'This is the Search Screen',
-          style: TextStyle(fontSize: 24),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('UserList'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                showSearch(context: context, delegate: SearchUser());
+              },
+              icon: Icon(Icons.search_sharp),
+            )
+          ],
+        ),
+        body: Container(
+          padding: EdgeInsets.all(20),
+          child: FutureBuilder<List<Userlist>>(
+              future: _userList.getuserList(),
+              builder: (context, snapshot) {
+                var data = snapshot.data;
+                return ListView.builder(
+                    itemCount: data?.length,
+                    itemBuilder: (context, index) {
+                      if (!snapshot.hasData) {
+                        return Center(child: CircularProgressIndicator());
+                      }
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ListTile(
+                            title: Row(
+                              children: [
+                                Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                    color: Colors.deepPurpleAccent,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      '${data?[index].id}',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 20),
+                                Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        '${data?[index].name}',
+                                        style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                      SizedBox(height: 10),
+                                      Text(
+                                        '${data?[index].email}',
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ])
+                              ],
+                            ),
+                            // trailing: Text('More Info'),
+                          ),
+                        ),
+                      );
+                    });
+              }),
         ),
       ),
     );
